a30b4947103074b068a4b360a882d5f264087150
aanggaadc <aanggaadc@gmail.com>
2022-05-13 09:02:15 +0700

    add react-router-dom path to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/Home'
-
 
 function App() {
   return (
-    <div>
-      <Home />
+    <div className="App">
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
