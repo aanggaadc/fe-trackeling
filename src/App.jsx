75432a12880b0d6ffea497f3bd8b1003e85c7c67
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-05-21 00:20:50 +0700

    done create login-signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/Home'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   return (
@@ -9,10 +11,12 @@ function App() {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
         </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
