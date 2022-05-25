6e8057f8f59ef327395d77775698550b806dfff6
Alifiandy Nugraha <alifiandyn@gmail.com>
2022-05-25 19:03:22 +0700

    Done add API for Detail Trip

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import MyTrip from "./pages/my_trip/MyTrip";
+import DetailTrip from "./pages/detail_trip/DetailTrip";
 import UserAccount from "./pages/user_account/UserAccount";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -12,28 +13,29 @@ import { ToastContainer } from "react-toastify";
 import PrivateRoutes from "./private_routes/PrivateRoutes";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/">
-					<Route index element={<Home />} />
-					<Route path="login" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
-					<Route element={<PrivateRoutes />}>
-						<Route path="trip">
-							<Route path="create" element={<TripForm />} />
-						</Route>
-						<Route path="user">
-							<Route path="edit/account/:userId" element={<UserAccount />} />
-							<Route path="edit/biodata/:userId" element={<UserAccount />} />
-							<Route path="mytrip/:userId" element={<MyTrip />} />
-						</Route>
-					</Route>
-				</Route>
-			</Routes>
-			<ToastContainer />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path="trip">
+              <Route path="create" element={<TripForm />} />
+              <Route path="detail/:tripId" element={<DetailTrip />} />
+            </Route>
+            <Route path="user">
+              <Route path="edit/account/:userId" element={<UserAccount />} />
+              <Route path="edit/biodata/:userId" element={<UserAccount />} />
+              <Route path="mytrip/:userId" element={<MyTrip />} />
+            </Route>
+          </Route>
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
