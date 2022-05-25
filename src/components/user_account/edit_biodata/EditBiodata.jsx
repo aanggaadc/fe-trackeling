b631e09060a3a7657188c5d31584553be19a1c28
deriscode <jdrepositories@gmail.com>
2022-05-25 15:06:27 +0700

    berhasil menampilkan data inputan di edit account

--- src/components/user_account/edit_biodata/EditBiodata.jsx
+++ src/components/user_account/edit_biodata/EditBiodata.jsx
@@ -6,115 +6,107 @@ import "./EditBiodata.css";
 import useAuth from "../../../utils/auth";
 
 function EditBiodata({ setFile }) {
-	const [initialValues, setInitialValues] = useState("");
-
 	const authData = useAuth();
 
 	return (
 		<div className="edit-biodata">
 			<h1>Edit Biodata</h1>
 			<Formik
-				initialValues={initialValues}
+				initialValues={{
+					phone_number: authData.profile.phone_number,
+					age: authData.profile.age,
+					location: authData.profile.location,
+					sex: authData.profile.sex,
+					interest: authData.profile.interest,
+				}}
 				onSubmit={(values) => {
 					console.log(values);
 				}}
 			>
-				<form action="" id="form-editBiodata">
-					<div className="form-row">
-						<div className="form-fullname">
-							<label for="exampleFormControlInput1" class="form-label">
-								Full Name
-							</label>
-							<input
-								type="text"
-								class="form-control"
-								id="exampleFormControlInput1"
-								placeholder="Michael Jackson"
-							/>
+				{({ values, handleSubmit, handleChange }) => (
+					<form action="" id="form-editBiodata">
+						<div className="form-row">
+							<div className="form-fullname">
+								<label for="fullname" class="form-label">
+									Full Name
+								</label>
+								<input type="text" class="form-control" id="fullname" placeholder="" />
+							</div>
+							<div className="form-phone">
+								<label for="phone" class="form-label">
+									Phone
+								</label>
+								<input
+									type="number"
+									class="form-control"
+									id="phone"
+									placeholder=""
+									value={values.phone_number}
+								/>
+							</div>
 						</div>
-						<div className="form-phone">
-							<label for="exampleFormControlInput1" class="form-label">
-								Phone
-							</label>
-							<input
-								type="number"
-								class="form-control"
-								id="exampleFormControlInput1"
-								placeholder="081234567890"
-							/>
+						<div className="form-row">
+							<div className="form-age">
+								<label for="age" class="form-label">
+									Age
+								</label>
+								<input type="number" class="form-control" id="age" placeholder="" />
+							</div>
+							<div className="form-location">
+								<label for="location" class="form-label">
+									Location
+								</label>
+								<input type="text" class="form-control" id="location" placeholder="" />
+							</div>
 						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-age">
-							<label for="exampleFormControlInput1" class="form-label">
-								Age
-							</label>
-							<input
-								type="number"
-								class="form-control"
-								id="exampleFormControlInput1"
-								placeholder="24"
-							/>
+						<div className="form-row">
+							<div className="form-sex">
+								<label for="specificSex" class="visually-hidden">
+									Sex
+								</label>
+								<select name="sex" id="specificSex">
+									<option value="">- Pick Your Sex -</option>
+									<option value="">Female</option>
+									<option value="">Male</option>
+								</select>
+							</div>
 						</div>
-						<div className="form-location">
-							<label for="exampleFormControlInput1" class="form-label">
-								Location
-							</label>
-							<input
-								type="text"
-								class="form-control"
-								id="exampleFormControlInput1"
-								placeholder="Bali"
-							/>
+						<div className="form-row">
+							<div className="form-interest">
+								<label for="specificInterest" class="visually-hidden">
+									Interest
+								</label>
+								<select name="interest" id="specificInterest">
+									<option value="">- Interest -</option>
+									<option value="">Music</option>
+									<option value="">Movie</option>
+									<option value="">Sport</option>
+									<option value="">Food</option>
+									<option value="">Photography</option>
+								</select>
+							</div>
 						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-sex">
-							<label for="specificSex" class="visually-hidden">
-								Sex
-							</label>
-							<select name="sex" id="specificSex">
-								<option value="">- Pick Your Sex -</option>
-								<option value="">Female</option>
-								<option value="">Male</option>
-							</select>
+						<div className="form-row">
+							<div className="form-avatar">
+								<label htmlFor="file">
+									<FaUpload /> Upload Avatar
+								</label>
+								<input
+									type="file"
+									id="file"
+									accept="image/*"
+									style={{ display: "none" }}
+									onChange={(e) => {
+										setFile(e.target.files[0]);
+									}}
+								/>
+							</div>
 						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-interest">
-							<label for="specificInterest" class="visually-hidden">
-								Interest
-							</label>
-							<select name="interest" id="specificInterest">
-								<option value="">- Interest -</option>
-								<option value="">Music</option>
-								<option value="">Movie</option>
-								<option value="">Sport</option>
-								<option value="">Food</option>
-								<option value="">Photography</option>
-							</select>
+						<div className="btn-submitBiodata">
+							<button type="submit">Submit</button>
 						</div>
-					</div>
-					<div className="form-row">
-						<div className="form-avatar">
-							<label htmlFor="file">
-								<FaUpload /> Upload Avatar
-							</label>
-							<input
-								type="file"
-								id="file"
-								accept="image/*"
-								style={{ display: "none" }}
-								onChange={(e) => {
-									setFile(e.target.files[0]);
-								}}
-							/>
-						</div>
-					</div>
-					<div className="btn-submitBiodata">
-						<button type="submit">Submit</button>
-					</div>
-				</form>
+					</form>
+				)}
 			</Formik>
 			<div className="change-formToAccount">
 				Change to{" "}
