dc532b674d273bace6b8620f8b84d460abcb10bc
RizkyPDA <muhammad.syadrie11@gmail.com>
2022-06-05 21:47:35 +0700

    hapus signup di page login admin

--- src/pages/login_admin/LoginAdmin.jsx
+++ src/pages/login_admin/LoginAdmin.jsx
@@ -13,107 +13,86 @@ import { bindActionCreators } from "redux";
 import { actionCreators } from "../../store/index";
 
 const renderTooltip = (props) => (
-	<Tooltip id="button-tooltip" {...props}>
-		Back to home
-	</Tooltip>
+  <Tooltip id="button-tooltip" {...props}>
+    Back to home
+  </Tooltip>
 );
 
 function LoginAdmin() {
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
-	const { fillUser } = bindActionCreators(actionCreators, dispatch);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { fillUser } = bindActionCreators(actionCreators, dispatch);
 
-	return (
-		<div id="background-login" className="content-login">
-			<div className="head-login">
-				<div className="logo">
-					<img src={Logo} alt="logo" />
-				</div>
-				<div className="title fw-bold">Admin</div>
+  return (
+    <div id="background-login" className="content-login">
+      <div className="head-login">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="title fw-bold">Admin</div>
 
-				<Link to="/">
-					<OverlayTrigger
-						placement="right"
-						delay={{ show: 250, hide: 400 }}
-						overlay={renderTooltip}
-					>
-						<Button variant="link">
-							<BsFillArrowLeftCircleFill />
-						</Button>
-					</OverlayTrigger>
-				</Link>
-			</div>
+        <Link to="/">
+          <OverlayTrigger placement="right" delay={{ show: 250, hide: 400 }} overlay={renderTooltip}>
+            <Button variant="link">
+              <BsFillArrowLeftCircleFill />
+            </Button>
+          </OverlayTrigger>
+        </Link>
+      </div>
 
-			<Formik
-				initialValues={{
-					email: "",
-					password: "",
-				}}
-				onSubmit={(values) => {
-					// Axios.post(`${API_URL}/login`, values)
-					// 	.then((response) => {
-					// 		fillUser(response.data.data);
-					// 		localStorage.setItem("authKey", JSON.stringify(response.data.data));
-					// 		navigate("/");
-					// 		toast.success("Welcome to Our Site!");
-					// 	})
-					// 	.catch((error) => {
-					// 		if (error.response) {
-					// 			toast.error(error.response.data.message);
-					// 		} else {
-					// 			toast.error("Can't Connect to Our Server");
-					// 		}
-					// 		console.log(error);
-					// 	});
-				}}
-			>
-				{({ handleSubmit, handleChange }) => (
-					<form id="form-login">
-						<div className="form-group">
-							<input
-								type="email"
-								className="form-control"
-								id="email"
-								name="email"
-								placeholder="Email"
-								onChange={handleChange}
-							/>
-							{/* <small id="emailHelp" className="text-danger form-text">
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          // Axios.post(`${API_URL}/login`, values)
+          // 	.then((response) => {
+          // 		fillUser(response.data.data);
+          // 		localStorage.setItem("authKey", JSON.stringify(response.data.data));
+          // 		navigate("/");
+          // 		toast.success("Welcome to Our Site!");
+          // 	})
+          // 	.catch((error) => {
+          // 		if (error.response) {
+          // 			toast.error(error.response.data.message);
+          // 		} else {
+          // 			toast.error("Can't Connect to Our Server");
+          // 		}
+          // 		console.log(error);
+          // 	});
+        }}
+      >
+        {({ handleSubmit, handleChange }) => (
+          <form id="form-login">
+            <div className="form-group">
+              <input type="email" className="form-control" id="email" name="email" placeholder="Email" onChange={handleChange} />
+              {/* <small id="emailHelp" className="text-danger form-text">
                 {emailError}
               </small> */}
-						</div>
+            </div>
 
-						<div className="form-group mt-3">
-							<input
-								type="password"
-								className="form-control"
-								id="password"
-								name="password"
-								placeholder="Password"
-								onChange={handleChange}
-							/>
-							{/* <small id="passworderror" className="text-danger form-text"></small> */}
-						</div>
+            <div className="form-group mt-3">
+              <input type="password" className="form-control" id="password" name="password" placeholder="Password" onChange={handleChange} />
+              {/* <small id="passworderror" className="text-danger form-text"></small> */}
+            </div>
 
-						<div className="btn-login">
-							<button onClick={handleSubmit} type="submit" className="btn btn-primary mt-3">
-								Login
-							</button>
-						</div>
-					</form>
-				)}
-			</Formik>
+            <div className="btn-login">
+              <button onClick={handleSubmit} type="submit" className="btn btn-primary mt-3">
+                Login
+              </button>
+            </div>
+          </form>
+        )}
+      </Formik>
 
-			<div className="login-text">
-				<p>
-					No Account? <Link to="/signup">Signup</Link>
-				</p>
-				<p>
-					Are you User? <Link to="/login">Login Here!</Link>
-				</p>
-			</div>
-		</div>
-	);
+      <div className="login-text">
+        <p>
+          Are you User? <Link to="/login">Login Here!</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LoginAdmin;
