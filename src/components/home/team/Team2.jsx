f4315d96d57fc4e9ed33db87c014e563d3503944
deriscode <jdrepositories@gmail.com>
2022-06-08 22:50:38 +0700

    memperbaiki responsif card trip & card member

--- src/components/home/team/Team2.jsx
+++ src/components/home/team/Team2.jsx
@@ -1,131 +1,207 @@
-import React from 'react'
-import './Team2.css'
+import React from "react";
+import "./Team2.css";
 import { BsTwitter, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 function Team2() {
-    return (
-        <section id="members">
-            <div className="container">
-                <div className="members-header">
-                    <h2>MEET THE TEAM</h2>
-                    <p>These are all extraordinary people who contributed to the creation of this website</p>
-                    <hr />
-                </div>
+	return (
+		<section id="members">
+			<div className="container">
+				<div className="members-header">
+					<h2>MEET THE TEAM</h2>
+					<p>These are all extraordinary people who contributed to the creation of this website</p>
+					<hr />
+				</div>
 
-                <div className="row d-flex justify-content-center">
-                    <div className="col-lg-4 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="juan" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 2" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 3" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 4" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 6" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+				<div className="row d-flex justify-content-center">
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="juan" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 2" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 3" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 4" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 6" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div className="col-lg-6 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 4" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-6 col-md-6">
-                        <div className="members">
-                            <img src="https://api.lorem.space/image/face" alt="Speaker 6" className="img-fluid" />
-                            <div className="details">
-                                <h3>Member</h3>
-                                <p>Developer</p>
-                                <div className="social">
-                                    <a href="#twitter"><BsTwitter /></a>
-                                    <a href="#github"><BsGithub /></a>
-                                    <a href="#instagram"><BsInstagram /></a>
-                                    <a href="#linkedid"><BsLinkedin /></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-        </section >
-
-    )
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 4" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 6" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-6">
+						<div className="members">
+							<img src="https://api.lorem.space/image/face" alt="Speaker 6" className="img-fluid" />
+							<div className="details">
+								<h3>Member</h3>
+								<p>Developer</p>
+								<div className="social">
+									<a href="#twitter">
+										<BsTwitter />
+									</a>
+									<a href="#github">
+										<BsGithub />
+									</a>
+									<a href="#instagram">
+										<BsInstagram />
+									</a>
+									<a href="#linkedid">
+										<BsLinkedin />
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
-export default Team2
+export default Team2;
