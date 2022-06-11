b1d554994c3106694d44a75050361edb8c379d5c
deriscode <jdrepositories@gmail.com>
2022-06-11 23:22:55 +0700

    add validation on all form

--- src/pages/create_trip/CreateTrip.jsx
+++ src/pages/create_trip/CreateTrip.jsx
@@ -1,160 +1,185 @@
-import React, { useState } from 'react'
-import './CreateTrip.css'
-import Navbar from '../../components/navbar/NavbarMain'
-import Footer from '../../components/footer/Footer'
-import { Formik, Form } from 'formik'
-import Axios from 'axios'
-import { toast } from 'react-toastify'
-import { API_URL } from '../../config/url'
+import React, { useState } from "react";
+import "./CreateTrip.css";
+import Navbar from "../../components/navbar/NavbarMain";
+import Footer from "../../components/footer/Footer";
+import { Formik, Form } from "formik";
+import Axios from "axios";
+import { toast } from "react-toastify";
+import { API_URL } from "../../config/url";
 import { useNavigate } from "react-router-dom";
 import { FaUpload } from "react-icons/fa";
-
+import moment from "moment";
 
 function CreateTrip() {
-    const navigate = useNavigate()
-    const [image, setImage] = useState("")
+	const navigate = useNavigate();
+	const [image, setImage] = useState("");
 
-    return (
-        <>
-            <Navbar />
-            <div className='tripform-background'>
-                <div className="container tripform-container">
-                    <div className='tripform-title text-center mt-3'>
-                        <h2>CREATE TRIP</h2>
-                    </div>
+	return (
+		<>
+			<Navbar />
+			<div className="tripform-background">
+				<div className="container tripform-container">
+					<div className="tripform-title text-center mt-3">
+						<h2>CREATE TRIP</h2>
+					</div>
 
-                    <div className="tripform">
-                        <Formik
-                            initialValues={{
-                                trip_name: "",
-                                destination: "",
-                                start_date: "",
-                                end_date: "",
-                                max_member: "",
-                                description: "",
-                                image: ""
-                            }}
+					<div className="tripform">
+						<Formik
+							initialValues={{
+								trip_name: "",
+								destination: "",
+								start_date: "",
+								end_date: "",
+								max_member: "",
+								description: "",
+								image: "",
+							}}
+							onSubmit={(values) => {
+								if (values.max_member < 2) {
+									toast.error("Max member must be minimal 2");
+									navigate("/trip/create");
+								} else if (values.start_date > values.end_date) {
+									toast.error("Please input the right date");
+									navigate("/trip/create");
+								} else {
+									const formData = new FormData();
+									formData.append("trip_name", values.trip_name);
+									formData.append("destination", values.destination);
+									formData.append("start_date", values.start_date);
+									formData.append("end_date", values.end_date);
+									formData.append("max_member", values.max_member);
+									formData.append("description", values.description);
+									formData.append("image", values.image);
 
-                            onSubmit={(values) => {
-                                const formData = new FormData()
-                                formData.append("trip_name", values.trip_name)
-                                formData.append("destination", values.destination)
-                                formData.append("start_date", values.start_date)
-                                formData.append("end_date", values.end_date)
-                                formData.append("max_member", values.max_member)
-                                formData.append("description", values.description)
-                                formData.append("image", values.image)
+									Axios.post(`${API_URL}/trip/add`, formData)
+										.then((response) => {
+											console.log(response);
+											toast.success("Trip Successfully created!!");
+											navigate("/");
+										})
+										.catch((error) => {
+											if (error.response) {
+												toast.error(error.response.data.message);
+											} else {
+												toast.error("Cannot Connect to Server");
+											}
+										});
+								}
+							}}
+						>
+							{({ handleSubmit, handleChange, setFieldValue }) => (
+								<Form id="form-trip">
+									<div className="form-group">
+										<label for="tripname">Trip Name</label>
+										<input
+											type="text"
+											className="form-control"
+											id="tripname"
+											name="trip_name"
+											onChange={handleChange}
+											required
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<label for="destination">Destination</label>
+										<input
+											type="text"
+											className="form-control"
+											id="destination"
+											name="destination"
+											onChange={handleChange}
+											required
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<label for="startdate">Start Date</label>
+										<input
+											type="date"
+											className="form-control"
+											id="startdate"
+											name="start_date"
+											placeholder="Start Date"
+											onChange={handleChange}
+											required
+											min={moment().add(1, "days").format("YYYY-MM-DD")}
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<label for="enddate">End Date</label>
+										<input
+											type="date"
+											className="form-control"
+											id="enddate"
+											name="end_date"
+											placeholder="End Date"
+											onChange={handleChange}
+											required
+											min={moment().add(1, "days").format("YYYY-MM-DD")}
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<label for="maxmember">Max Member</label>
+										<input
+											type="number"
+											className="form-control"
+											id="maxmember"
+											name="max_member"
+											onChange={handleChange}
+											required
+											min={2}
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<input
+											type="file"
+											id="image"
+											name="image"
+											accept="image/*"
+											style={{ position: "absolute", opacity: 0, cursor: "pointer" }}
+											onChange={(e) => {
+												setImage(e.target.files[0]);
+												setFieldValue("image", e.currentTarget.files[0]);
+											}}
+											required
+										/>
+										<label for="file">
+											{" "}
+											<FaUpload size={22} /> Upload Trip Image{" "}
+										</label>{" "}
+										<br /> <br />
+										<img
+											src={
+												image
+													? URL.createObjectURL(image)
+													: "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+											}
+											alt="trip image"
+											className="img-fluid image-trip"
+										/>
+									</div>
+									<div className="form-group mt-3">
+										<textarea
+											name="description"
+											placeholder="Trip Description"
+											onChange={handleChange}
+											required
+										/>
+									</div>
 
-                                Axios.post(`${API_URL}/trip/add`, formData)
-                                    .then((response) => {
-                                        console.log(response)
-                                        toast.success("Trip Successfully created!!")
-                                        navigate('/')
-                                    })
-                                    .catch((error) => {
-                                        if (error.response) {
-                                            toast.error(error.response.data.message)
-                                        } else {
-                                            toast.error("Cannot Connect to Server")
-                                        }
-                                    })
-                            }}
-                        >
-                            {({ handleSubmit, handleChange, setFieldValue }) => (
-                                <Form id="form-trip">
-                                    <div className="form-group">
-                                        <label for='tripname' >Trip Name</label>
-                                        <input type="text"
-                                            className="form-control"
-                                            id="tripname"
-                                            name="trip_name"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <label for='destination' >Destination</label>
-                                        <input type="text"
-                                            className="form-control"
-                                            id="destination"
-                                            name="destination"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <label for='startdate' >Start Date</label>
-                                        <input type="date"
-                                            className="form-control"
-                                            id="startdate"
-                                            name="start_date"
-                                            placeholder="Start Date"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <label for='enddate' >End Date</label>
-                                        <input type="date"
-                                            className="form-control"
-                                            id="enddate"
-                                            name="end_date"
-                                            placeholder="End Date"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <label for='maxmember' >Max Member</label>
-                                        <input type="number"
-                                            className="form-control"
-                                            id="maxmember"
-                                            name="max_member"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <input type='file'
-                                            id='image'
-                                            name="image"
-                                            accept='image/*'
-                                            style={{ position: "absolute", opacity: 0, cursor: "pointer" }}
-                                            onChange={(e) => {
-                                                setImage(e.target.files[0])
-                                                setFieldValue('image', e.currentTarget.files[0])
-                                            }}
-                                            required />
-                                        <label for='file'> <FaUpload size={22} /> Upload Trip Image </label> <br /> <br />
-                                        <img
-                                            src={
-                                                image
-                                                    ? URL.createObjectURL(image)
-                                                    : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-                                            }
-                                            alt="trip image"
-                                            className="img-fluid image-trip"
-                                        />
-                                    </div>
-                                    <div className="form-group mt-3">
-                                        <textarea
-                                            name="description"
-                                            placeholder="Trip Description"
-                                            onChange={handleChange}
-                                            required />
-                                    </div>
-
-                                    <div className="btn-create mt-3">
-                                        <button onClick={handleSubmit} type="submit" className="btn btn-primary mt-3">
-                                            CREATE
-                                        </button>
-                                    </div>
-                                </Form>
-                            )}
-                        </Formik>
-                    </div>
-                </div>
-
-            </div>
-            <Footer />
-        </>
-
-    )
+									<div className="btn-create mt-3">
+										<button onClick={handleSubmit} type="submit" className="btn btn-primary mt-3">
+											CREATE
+										</button>
+									</div>
+								</Form>
+							)}
+						</Formik>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
-export default CreateTrip
+export default CreateTrip;
--- src/pages/detail_recommendation_trip/DetailRecommendationTrip.jsx
+++ src/pages/detail_recommendation_trip/DetailRecommendationTrip.jsx
@@ -11,186 +11,241 @@ import OtherTripList from "../../components/other_trip/OtherTrip";
 import NoData from "../../no-data.gif";
 import { RiArrowRightCircleFill } from "react-icons/ri";
 import { Formik, Form } from "formik";
+import moment from "moment";
 
 function DetailRecommendationTrip() {
-  const { recommendationId } = useParams();
-  const navigate = useNavigate();
-  const [dataOtherTrip, setDataOtherTrip] = useState([]);
-  const [trip, setTrip] = useState({});
+	const { recommendationId } = useParams();
+	const navigate = useNavigate();
+	const [dataOtherTrip, setDataOtherTrip] = useState([]);
+	const [trip, setTrip] = useState({});
 
-  // For Modal Operation
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  // For Modal Operation
+	// For Modal Operation
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+	// For Modal Operation
 
-  const getDataOtherTrip = () => {
-    Axios.get(`${API_URL}/trip/other_trip/${recommendationId}`)
-      .then((response) => {
-        setDataOtherTrip(response.data.data);
-      })
-      .catch((error) => {
-        console.log(error.data.message);
-      });
-  };
+	const getDataOtherTrip = () => {
+		Axios.get(`${API_URL}/trip/other_trip/${recommendationId}`)
+			.then((response) => {
+				setDataOtherTrip(response.data.data);
+			})
+			.catch((error) => {
+				console.log(error.data.message);
+			});
+	};
 
-  useEffect(() => {
-    Axios.get(`${API_URL}/recomendation/detail/${recommendationId}`)
-      .then((response) => {
-        const apiData = response.data.data;
-        setTrip({
-          recomendation_id: apiData.recomendation_id,
-          adminMainId: apiData.adminMainId,
-          trip_image: API_URL + apiData.trip_image,
-          destination: apiData.destination,
-          description: apiData.description,
-        });
-      })
-      .catch((error) => {
-        if (error.response) {
-          toast.error(error.response.data.message);
-        } else {
-          toast.error("Something Wrong");
-        }
-        navigate("/");
-      });
-    getDataOtherTrip();
-  }, [recommendationId]);
+	useEffect(() => {
+		Axios.get(`${API_URL}/recomendation/detail/${recommendationId}`)
+			.then((response) => {
+				const apiData = response.data.data;
+				setTrip({
+					recomendation_id: apiData.recomendation_id,
+					adminMainId: apiData.adminMainId,
+					trip_image: API_URL + apiData.trip_image,
+					destination: apiData.destination,
+					description: apiData.description,
+				});
+			})
+			.catch((error) => {
+				if (error.response) {
+					toast.error(error.response.data.message);
+				} else {
+					toast.error("Something Wrong");
+				}
+				navigate("/");
+			});
+		getDataOtherTrip();
+	}, [recommendationId]);
 
-  const OtherTrip = () => {
-    if (dataOtherTrip.length > 0) {
-      return (
-        <>
-          <OtherTripList data={dataOtherTrip} />
-          <Link style={{ textDecoration: "none", color: "#188CBD", fontSize: "20px" }} className="float-end mt-3 link-trip" to="/trips">
-            See Other
-            <RiArrowRightCircleFill size={30} />
-          </Link>
-        </>
-      );
-    } else {
-      return <img className="img-fluid" style={{ width: "500px" }} src={NoData} alt="No-data" />;
-    }
-  };
+	const OtherTrip = () => {
+		if (dataOtherTrip.length > 0) {
+			return (
+				<>
+					<OtherTripList data={dataOtherTrip} />
+					<Link
+						style={{ textDecoration: "none", color: "#188CBD", fontSize: "20px" }}
+						className="float-end mt-3 link-trip"
+						to="/trips"
+					>
+						See Other
+						<RiArrowRightCircleFill size={30} />
+					</Link>
+				</>
+			);
+		} else {
+			return <img className="img-fluid" style={{ width: "500px" }} src={NoData} alt="No-data" />;
+		}
+	};
 
-  return (
-    <div className="bg-content">
-      <NavbarMain />
-      <Container className="detailtrip-container py-5">
-        <Row>
-          <Col lg="5">
-            <Card style={{ width: "100%" }}>
-              <Image src={trip.trip_image} className="img-fluid rounded shadow-4 detail-trip-img" alt="..." />
-            </Card>
-          </Col>
-          <Col lg>
-            <h1>{trip.destination}</h1>
-            <div>
-              <Row className="justify-content-start mx-0 my-4">
-                <div className="p-0">
-                  <Button className="btn" variant="primary" onClick={handleShow} active>
-                    CREATE THIS TRIP
-                  </Button>
-                </div>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-        <hr className="my-5" />
-        <Row className="mb-3">
-          <h3>Description</h3>
-          <br />
-          <p>{trip.description}</p>
-        </Row>
-        <Row className="mt-3">
-          <Col lg="4">
-            <h3>Posted By</h3>
-            <div className="posted">
-              <Image src={"https://cdn-icons-png.flaticon.com/512/147/147142.png"} className="img-fluid rounded-circle shadow-4 image-profile" alt="..." />
-              <div className="name-profile">
-                <p className="mb-0">
-                  <b>Admin</b>
-                </p>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <hr className="my-5" />
-        <Row>
-          <div className="other-trip">
-            <h2>READY TRIP</h2>
-            <p>Trips made by the community</p>
-          </div>
-          <div className="text-center">{OtherTrip()}</div>
-        </Row>
-      </Container>
-      <Footer />
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>CREATE TRIP</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Formik
-            initialValues={{
-              trip_name: "",
-              destination: trip.destination,
-              start_date: "",
-              end_date: "",
-              max_member: "",
-              description: trip.description,
-              image: trip.trip_image,
-            }}
-            onSubmit={(values) => {
-              Axios.post(`${API_URL}/trip/add_recommendation`, values)
-                .then((response) => {
-                  console.log(response);
-                  toast.success("Trip Successfully created!!");
-                  navigate("/");
-                })
-                .catch((error) => {
-                  if (error.response) {
-                    toast.error(error.response.data.message);
-                  } else {
-                    toast.error("Cannot Connect to Server");
-                  }
-                });
-            }}
-          >
-            {({ handleSubmit, handleChange }) => (
-              <Form>
-                <div className="form-group">
-                  <label for="tripname">Trip Name</label>
-                  <input type="text" className="form-control" id="tripname" name="trip_name" onChange={handleChange} required />
-                </div>
-                <div className="form-group mt-3">
-                  <label for="startdate">Start Date</label>
-                  <input type="date" className="form-control" id="startdate" name="start_date" placeholder="Start Date" onChange={handleChange} required />
-                </div>
-                <div className="form-group mt-3">
-                  <label for="enddate">End Date</label>
-                  <input type="date" className="form-control" id="enddate" name="end_date" placeholder="End Date" onChange={handleChange} required />
-                </div>
-                <div className="form-group mt-3">
-                  <label for="maxmember">Max Member</label>
-                  <input type="number" className="form-control" id="maxmember" name="max_member" onChange={handleChange} required />
-                </div>
-                <div className="btn-create">
-                  <button onClick={handleSubmit} type="submit" className="btn btn-primary w-100 mt-3">
-                    Create Trip
-                  </button>
-                </div>
-              </Form>
-            )}
-          </Formik>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
+	return (
+		<div className="bg-content">
+			<NavbarMain />
+			<Container className="detailtrip-container py-5">
+				<Row>
+					<Col lg="5">
+						<Card style={{ width: "100%" }}>
+							<Image
+								src={trip.trip_image}
+								className="img-fluid rounded shadow-4 detail-trip-img"
+								alt="..."
+							/>
+						</Card>
+					</Col>
+					<Col lg>
+						<h1>{trip.destination}</h1>
+						<div>
+							<Row className="justify-content-start mx-0 my-4">
+								<div className="p-0">
+									<Button className="btn" variant="primary" onClick={handleShow} active>
+										CREATE THIS TRIP
+									</Button>
+								</div>
+							</Row>
+						</div>
+					</Col>
+				</Row>
+				<hr className="my-5" />
+				<Row className="mb-3">
+					<h3>Description</h3>
+					<br />
+					<p>{trip.description}</p>
+				</Row>
+				<Row className="mt-3">
+					<Col lg="4">
+						<h3>Posted By</h3>
+						<div className="posted">
+							<Image
+								src={"https://cdn-icons-png.flaticon.com/512/147/147142.png"}
+								className="img-fluid rounded-circle shadow-4 image-profile"
+								alt="..."
+							/>
+							<div className="name-profile">
+								<p className="mb-0">
+									<b>Admin</b>
+								</p>
+							</div>
+						</div>
+					</Col>
+				</Row>
+				<hr className="my-5" />
+				<Row>
+					<div className="other-trip">
+						<h2>READY TRIP</h2>
+						<p>Trips made by the community</p>
+					</div>
+					<div className="text-center">{OtherTrip()}</div>
+				</Row>
+			</Container>
+			<Footer />
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>CREATE THIS TRIP</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Formik
+						initialValues={{
+							trip_name: "",
+							destination: trip.destination,
+							start_date: "",
+							end_date: "",
+							max_member: "",
+							description: trip.description,
+							image: trip.trip_image,
+						}}
+						onSubmit={(values) => {
+							if (values.max_member < 2) {
+								toast.error("Max member must be minimal 2");
+							} else if (values.start_date > values.end_date) {
+								toast.error("Please input the right date");
+							} else {
+								Axios.post(`${API_URL}/trip/add_recommendation`, values)
+									.then((response) => {
+										console.log(response);
+										toast.success("Trip Successfully created!!");
+										navigate("/");
+									})
+									.catch((error) => {
+										if (error.response) {
+											toast.error(error.response.data.message);
+										} else {
+											toast.error("Cannot Connect to Server");
+										}
+									});
+							}
+						}}
+					>
+						{({ handleSubmit, handleChange }) => (
+							<Form>
+								<div className="form-group">
+									<label for="tripname">Trip Name</label>
+									<input
+										type="text"
+										className="form-control"
+										id="tripname"
+										name="trip_name"
+										onChange={handleChange}
+										required
+									/>
+								</div>
+								<div className="form-group mt-3">
+									<label for="startdate">Start Date</label>
+									<input
+										type="date"
+										className="form-control"
+										id="startdate"
+										name="start_date"
+										placeholder="Start Date"
+										onChange={handleChange}
+										required
+										min={moment().add(1, "days").format("YYYY-MM-DD")}
+									/>
+								</div>
+								<div className="form-group mt-3">
+									<label for="enddate">End Date</label>
+									<input
+										type="date"
+										className="form-control"
+										id="enddate"
+										name="end_date"
+										placeholder="End Date"
+										onChange={handleChange}
+										required
+										min={moment().add(1, "days").format("YYYY-MM-DD")}
+									/>
+								</div>
+								<div className="form-group mt-3">
+									<label for="maxmember">Max Member</label>
+									<input
+										type="number"
+										className="form-control"
+										id="maxmember"
+										name="max_member"
+										onChange={handleChange}
+										required
+									/>
+								</div>
+								<div className="btn-create">
+									<button
+										onClick={handleSubmit}
+										type="submit"
+										className="btn btn-primary w-100 mt-3"
+									>
+										Create Trip
+									</button>
+								</div>
+							</Form>
+						)}
+					</Formik>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant="secondary" onClick={handleClose}>
+						Close
+					</Button>
+				</Modal.Footer>
+			</Modal>
+		</div>
+	);
 }
 
 export default DetailRecommendationTrip;
