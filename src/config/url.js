a0434e959d175277ac4e6767fcd60f8775ca1833
deriscode <jdrepositories@gmail.com>
2022-06-16 17:03:10 +0700

    change api url part 2

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,5 @@
-export const API_URL = process.env.REACT_APP_API_URL
-export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL
+// export const API_URL = process.env.REACT_APP_API_URL
+// export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL
+
+export const API_URL = "https://api.trackeling.xyz";
+export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL;
