6fb3e0635ebad555a6ba58cd558b93fd825532c0
deriscode <jdrepositories@gmail.com>
2022-06-16 17:09:49 +0700

    change api & admin url

--- src/config/url.js
+++ src/config/url.js
@@ -2,4 +2,4 @@
 // export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL
 
 export const API_URL = "https://api.trackeling.xyz";
-export const ADMIN_URL = process.env.REACT_APP_ADMIN_URL;
+export const ADMIN_URL = "https://admin.trackeling.xyz";
